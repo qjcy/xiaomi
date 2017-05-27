174f8bf06b0150b44a384f52e4872d1f1154ca6f
lqj <573413948@qq.com>
2017-05-27 17:44:18 +0800

    ...

--- js/index.js
+++ js/index.js
@@ -18,5 +18,21 @@ $(function () {
         },300);
         $('.shopping-detail').slideUp('fast');
     });
+    
+    
+    //搜索框
+    $('#search-text').focus(function () {
+        $('.content-top .hot-word').css('display','none');
+        $(this).css('border-color','#FF6700');
+        $('#search-btn').css('border-color','#FF6700');
+        $('.content-top .search-detail').show();
+    });
+
+    $('#search-text').blur(function () {
+        $('.content-top .hot-word').css('display','inline-block');
+        $(this).css('border-color','#E0E0E0');
+        $('#search-btn').css('border-color','#E0E0E0');
+        $('.content-top .search-detail').hide(100);
+    })
 
 });
